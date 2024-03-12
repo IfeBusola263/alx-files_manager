327279c1aa58f15241fb7bbe53ef6ccfaad09f33
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-03-13 00:37:52 +0100

    mend

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,18 @@
-import redisClient from '../utils/redis.js';
-import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 export default class AppController {
-    static getStatus(req, res) {
-	if (redisClient.isAlive() && dbClient.isAlive()) {
-	    res.status(200).json({ "redis": true, "db": true });
-	    return;
-	} else {
-	    res.status(500);
-	}
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.status(200).json({ redis: true, db: true });
+    } else {
+      res.status(500);
     }
+  }
 
-    static async getStats(req, res) {
-	const numUsers = await dbClient.nbUsers();
-	const numFiles = await dbClient.nbFiles();
-	res.status(200).json({ "users": numUsers, "files": numFiles });
-    }
+  static async getStats(req, res) {
+    const numUsers = await dbClient.nbUsers();
+    const numFiles = await dbClient.nbFiles();
+    res.status(200).json({ users: numUsers, files: numFiles });
+  }
 }
