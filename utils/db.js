dc4df004e65040d175f4e60a8b1c29f9c1542cb8
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-03-13 00:15:50 +0100

    fixed the mongodb issue

--- utils/db.js
+++ utils/db.js
@@ -1,48 +1,39 @@
 import { MongoClient } from 'mongodb';
 
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}`;
+
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${this.host}:${this.port}`;
     this.client = new MongoClient(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   isAlive() {
-    try {
-      this.client.connect();
-      return true;
-    } catch (err) {
-      return false;
-    }
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    // try {
-    await this.client.connect();
-    const db = this.client.db(this.database);
-    const userCollections = db.collections('users');
-    const numDocs = await userCollections.countDocuments({});
+    const userCollections = this.db.collection('users');
+    const numDocs = await userCollections.countDocuments();
     return numDocs;
-    // } catch(error) {
-    //     console.log(error);
-    // }
   }
 
   async nbFiles() {
-    // try {
-    await this.client.connect();
-    const db = this.client.db(this.database);
-    const fileCollections = db.collections('files');
-    const numFiles = await fileCollections.countDocuments({});
+    const fileCollections = this.db.collection('files');
+    const numFiles = await fileCollections.countDocuments();
     return numFiles;
-    // } catch(error) {
-    //     console.log(error);
-    // }
   }
 }
 
